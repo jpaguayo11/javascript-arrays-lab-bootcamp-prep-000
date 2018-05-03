06a19185c6f1d84b959bf7baa05f85131e2cb2f0
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-03 15:52:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function prependKitten(name) {
   return newArray
 }
 function removeLastKitten() {
-  var newArray = [kittens.slice(0,1)]
+  var newArray = [kittens.slice(0,2)]
   return newArray
 }
